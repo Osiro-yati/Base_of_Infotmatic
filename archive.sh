e83f7fea72f0dc7e429b1cbc199ddbc90fa2971e
Osiro-yati <135268773+Osiro-yati@users.noreply.github.com>
2024-12-11 14:32:47 +0300

    Update archive.sh

--- archive.sh
+++ archive.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+archive_large_files() {
+    local directory="$1"
+    local max_size="$2"
+    local archive_name="archive.tar"
+
+    tar -cf "$archive_name" -C "$directory" $(find "$directory" -type f -size +"${max_size}c")
+
+    if [ $? -eq 0 ]; then
+        echo "Архив успешно создан: $archive_name"
+    else
+        echo "Ошибка при создании архива."
+    fi
+}
+
+if [ "$#" -ne 2 ]; then
+    echo "Использование: $0 путь_к_директории максимальный_размер_файла"
+    exit 1
+fi
+
 DIRECTORY="$1"
 MAX_SIZE="$2"
 
-tar -cf archive.tar -C "$DIRECTORY" $(find "$DIRECTORY" -type f -size +"${MAX_SIZE}c")
-
-if [ $? -eq 0 ]; then
-    echo "Ошибка при создании архива."
-else
-    echo "Архив создан: archive.tar"
-fi
+archive_large_files "$DIRECTORY" "$MAX_SIZE"
